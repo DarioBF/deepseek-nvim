b42d7e154e480c1fc4c0b0099fa42072d191a02d
dariobf <contacto@dariobf.com>
2025-01-22 20:04:57 +0100

    0.1.2

--- lua/deepseek/init.lua
+++ lua/deepseek/init.lua
@@ -1,2 +1,10 @@
 -- init.lua
-require('deepseek')
+local M = {}
+
+-- Cargar el módulo principal
+M.setup = function(user_config)
+	require('deepseek.deepseek').setup(user_config)
+end
+
+return M
+
